66811c8b513b0fc50045fd0eb7bf218ab8f36b72
Nathan Hodnett <nathanhodnett@gmail.com>
2021-10-21 11:37:07 -0600

    Add Airport Model Object

---
+++ models/modelA.js
@@ -0,0 +1,11 @@
+var airport =
+{
+  name: 'Denver International Airport',
+  nearestCity: 'Denver, CO',
+  numberOfRunways: '6',
+  numberOfTerminals: '3',
+  isBusy: true,
+  isDirty: false,
+  guestServices: ['Restrooms', 'First-Aid', 'Information']
+  terminalNames: ['A', 'B', 'C']
+}
