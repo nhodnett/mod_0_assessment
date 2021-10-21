7af7c15c928485732a9ff9a2070eeeffcdfa2b31
Nathan Hodnett <nathanhodnett@gmail.com>
2021-10-21 11:42:47 -0600

    Add Vehicle Model Object

---
+++ models/modelB.js
@@ -0,0 +1,11 @@
+var vehicle =
+{
+  make: 'Subaru',
+  model: 'Crosstrek',
+  numberOfDoors: 5,
+  numberOfWheels: 4,
+  isGrey: true,
+  isCommonInColorado: true,
+  niceFeatures: ['Air Conditioning', '4WD', 'Alloy Wheels'],
+  safetyFeatures: ['Air Bags', 'Lane Departure Warning', 'Reverse Parking Camera']
+  }
