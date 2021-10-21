16be8af059f288ddbef26d3e150c26373d6ae6a0
Nathan Hodnett <nathanhodnett@gmail.com>
2021-10-21 11:44:06 -0600

    Correct last line of code

--- models/modelA.js
+++ models/modelA.js
@@ -6,6 +6,6 @@ var airport =
   numberOfTerminals: '3',
   isBusy: true,
   isDirty: false,
-  guestServices: ['Restrooms', 'First-Aid', 'Information']
+  guestServices: ['Restrooms', 'First-Aid', 'Information'],
   terminalNames: ['A', 'B', 'C']
 }
