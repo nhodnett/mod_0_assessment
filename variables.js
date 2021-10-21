f8379d7296884ea02b3b948f2647e88cb87987d6
Nathan Hodnett <nathanhodnett@gmail.com>
2021-10-21 11:27:30 -0600

    Complete program specific challenge

---
+++ variables.js
@@ -0,0 +1,9 @@
+var famousPerson = 'Tom Cruise';
+var isActive = true;
+famousPerson = 'Madonna';
+var num3 = 5;
+var num9 = 6;
+var sum = num3+num9;
+var animals = ['Skunk', 'Fox', 'Raccoon'];
+animals.shift();
+famousPerson.length;
